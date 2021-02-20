cac9077a44c084f584ea4a5a3b1ad7fb651e2ffe
kota <kota200016@icloud.com>
2021-02-20 17:07:57 +0900

    create config

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,14 @@
 import React, {useState} from 'react';
 import SignIn from "./SignIn";
 import Main from "./Main";
+import config from "../config.json"
 
 export default () => {
     const [name, setName] = useState("");
 
-    if (name === '') {
+    if (name === '' && config.signInAbled) {
         return <SignIn setName={setName} />;
     } else {
         return <Main name={name} />
-    }
+    };
 };
