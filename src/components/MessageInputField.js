e381ae27f8c96630aec2e02199a302c0fe725011
kota <kota200016@icloud.com>
2021-02-21 12:50:46 +0900

    generate avatar

--- src/components/MessageInputField.js
+++ src/components/MessageInputField.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { makeStyles, } from '@material-ui/core/styles';
 import { Grid, Avatar } from '@material-ui/core';
+import { gravatarPath } from '../gravatar';
 
 const useStyles = makeStyles({
     root: {
@@ -9,20 +10,21 @@ const useStyles = makeStyles({
     }
 });
 
-const MessageInputField = () => {
+const MessageInputField = ({name}) => {
     const classes = useStyles();
+    const avatarPath = gravatarPath(name);
 
     return (
         <div className={classes.root}>
            <Grid container>
                 <Grid xs={1}>
-                    <Avatar alt=""/>
+                    <Avatar alt="" src={avatarPath}/>
                 </Grid>
                 <Grid xs={10}>
-
+                    afiafjaj
                 </Grid>
                 <Grid xs={1}>
-
+                    fasjasn
                 </Grid>
            </Grid>
         </div>
---
+++ src/gravatar.js
@@ -0,0 +1,8 @@
+import crypto from "crypto";
+//サインインページで入力されたニックネームが引数に渡され、空白除去、小文字化を行い、md5でハッシュを生成し、digestで適当な16進数を取得。それをgravatarのURLに埋め込んでアイコンを表示させる関数。
+export const gravatarPath = (string) => {
+    const lowerCaseString = string.trim().toLowerCase();
+    const md5 = crypto.createHash('md5');
+    const digest = md5.update(lowerCaseString, "binary").digest('hex');
+    return `https://www.gravatar.com/avatar${digest}/?=robohash`;
+};
